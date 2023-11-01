3f673f208316c377989f05c52c6ceb59cbbcb82f
Saba Phruidze <sabaphruidze01@gmail.com>
2023-11-01 14:32:07 +0400

    I copy pasted my previus project to test it here

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useState } from "react";
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "shaun", id: 1 },
+    { name: "yoshi", id: 2 },
+    { name: "mario", id: 3 },
+    { name: "luigi", id: 4 },
+    { name: "peach", id: 5 },
+    { name: "toad", id: 6 },
+    { name: "bowser", id: 7 },
+    { name: "aaron", id: 8 },
+    { name: "specter", id: 9 },
+    { name: "lit", id: 10 },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id} // this will become the key
+        data={people}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     </View>
   );
 }
@@ -13,8 +29,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
